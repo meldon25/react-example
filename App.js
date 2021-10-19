3a5020f491d0ebdd59555635d3f09d31b91e9166
Mel Donegan <mdonegan@mediafly.com>
2021-10-19 09:49:18 -0500

    added expense forms

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import NewExpense from "./components/NewExpense/NewExpense"
+import Expenses from "./components/Expenses/Expenses"
+
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible</p>
-    </div>
-  );
+
+	const expenses = [
+		{
+				title: 'Toilet Paper',
+				amount: 94.12,
+				date: new Date(2020, 7, 14),
+			},
+			{ 
+				title: 'New TV', 
+				amount: 799.49, 
+				date: new Date(2021, 2, 12) 
+		},
+			{
+				title: 'Car Insurance',
+				amount: 294.67,
+				date: new Date(2021, 2, 28),
+			},
+			{
+				title: 'New Desk (Wooden)',
+				amount: 450,
+				date: new Date(2021, 5, 12),
+			},
+]
+
+	const addExpenseHandler = expense => {
+		console.log(expense)
+
+	}
+
+// return React.createElement(
+// 	'div', 
+// 	{}, 
+// 	React.createElement('h2', {}, "Let's get started!")),
+// 	React.createElement(Expenses, {items: expenses}
+// 	)
+
+	return (
+	  <>
+	  <NewExpense onAddExpense={addExpenseHandler} />
+	  <Expenses items={expenses} />
+	  </>
+	);
 }
 
 export default App;
